613202a654ff4afa5495c9175d8df57b4a4ebb52
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2021-08-23 14:20:03 +0200

    Update style.js

--- style.js
+++ style.js
@@ -26,6 +26,7 @@
             	{ name: 'Lista Desordenada 1', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-1'}}, 
 
             	{ name: 'Celda 1', element: 'td', attributes: { 'class': 'bck-td-1'} },
+		    { name: 'Celda 2', element: 'td', attributes: { 'class': 'bck-td-2'} },
 
             	{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-1'}}
             	]
